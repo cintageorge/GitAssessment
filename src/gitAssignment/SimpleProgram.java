14ed309ee4af97a41def781a6dafc1622349921d
Cinta George <cintageorge1995@gmail.com>
2025-11-29 11:49:10 +0530

    Initial commit

---
+++ src/gitAssignment/SimpleProgram.java
@@ -0,0 +1,13 @@
+package gitAssignment;
+
+public class SimpleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a sample program ");
+		int a=5,b=5;
+		int sum=a+b;
+		System.out.println("Total: "+ sum);
+	}
+
+}
